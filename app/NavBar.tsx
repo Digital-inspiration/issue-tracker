b2ac19fd5dd911b66edeb3db3307a9597b9e7e14
digitally.inspires <digitally.inspires@gmail.com>
2024-01-03 01:07:15 +1030

    Create a NavBar Create a prisma issue model Create a local mysql db

---
+++ app/NavBar.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { ImBug } from "react-icons/im";
+import classNames from "classnames";
+
+
+const NavBar = () => {
+
+    const currentPath = usePathname();
+    console.log(currentPath);
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+    { label: "Contact", href: "/contact" },
+  ];
+
+  return (
+    <nav className="flex space-x-5 border-b px-5 h-14 items-center">
+      <Link href="/">
+        <ImBug />
+      </Link>
+      <ul className="flex space-x-5">
+        {links.map((link) => (
+          <li>
+            <Link
+              key={link.href}
+              className={classNames({
+                'text-zinc-950': currentPath === link.href,
+                'text-zinc-500': currentPath !== link.href,
+                'hover: text-zinc-700 transition-colors': true
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
