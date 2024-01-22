e9fabd0bdbcdea996f8e597ed705605bcb1f7c6c
digitally.inspires <digitally.inspires@gmail.com>
2024-01-22 23:42:44 +1030

    Extract the ErrorMessage component

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@radix-ui/themes'
+import React, { PropsWithChildren } from 'react'
+
+const ErrorMessage = ({children}: PropsWithChildren) => {
+    if (!children) return null;
+    
+  return (
+    <Text color='red' as= "p" className='text-xs'>{children}</Text>
+  )
+}
+
+export default ErrorMessage
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,13 +10,14 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: {errors} } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const { register, control, handleSubmit, formState: { errors }  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
 
@@ -42,7 +43,7 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && <Text color="red" as= "p" className="text-xs">{errors.title.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
 
         <Controller
           name="description"
@@ -51,7 +52,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && <Text color="red" as= "p" className="text-xs">{errors.description.message}</Text>}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
